d2dea79b10a8c4329e457aed4cf48cc916c4f925
geore <geore@LAPTOP-G6MS9ON9>
2020-03-01 20:16:18 +0200

    added file operations

---
+++ MathOperations/Operations.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace MathOperations
+{
+    class Operations
+    {
+        public void CalculateDigitSum()
+        {
+            Console.WriteLine("Please enter a number to check it's digit sum");
+
+            int digitsum = 0;
+            int number = int.Parse(Console.ReadLine());
+
+            // NOT accounted numbers lower than 10.
+            while (number > 10)
+            {
+                digitsum += (number % 10);
+                number /= 10;
+
+                if (number < 10)
+                {
+                    digitsum += number;
+                }
+            }
+
+            Console.WriteLine("The sum of digits is: " + digitsum);
+        }
+        public void CalculateFactorial()
+        {
+            Console.WriteLine("Please enter a number to check it's factorial");
+
+            int number = int.Parse(Console.ReadLine());
+            int factorial = 1;
+            for (int i = 1; i <= number; i++)
+            {
+
+                factorial *= i;
+            }
+
+            Console.WriteLine("The Factorial of " + number + " is: " + factorial);
+        }
+        public void IsPrime()
+        {
+            Console.WriteLine("Please enter a number to check whether is Prime or not");
+            int number = int.Parse(Console.ReadLine());
+
+            int Dividers = 0;
+
+            for (int i = 1; i <= number; i++)
+            {
+                if (number % i == 0)
+                {
+                    Dividers++;
+                }
+            }
+
+            if (Dividers > 2)
+            {
+                Console.WriteLine("The number " + number + " is not a Prime number");
+            }
+            else
+            {
+                Console.WriteLine("The number " + number + " is a Prime number");
+            }
+        }
+    }
+}
--- MathOperations/Program.cs
+++ MathOperations/Program.cs
@@ -7,26 +7,39 @@ namespace MathOperations
         static void Main(string[] args)
         {
             // Users choice
+            
+            Operations operation = new Operations();
+            Initializer();
             int _choice;
+            _choice = int.Parse(Console.ReadLine());
+
+            switch (_choice)
+            {
+                case 2:
+                    operation.IsPrime();
+                    break;
+                case 4:
+                    operation.CalculateDigitSum();
+                    break;
+                case 6:
+                    operation.CalculateFactorial();
+                    break;
+            }
+
+        }
+
+
+        private static void Initializer()
+        {
             // Select Mathematical Operation
             Console.WriteLine("Please select a number for your corresponding operation\n");
             Console.WriteLine("1.Fibbonacci Series");
-            Console.WriteLine("2.Prime Numbers");
+            Console.WriteLine("2.Prime Numbers");//Done
             Console.WriteLine("3.Armstrong Number");
-            Console.WriteLine("4.Digit Sum");
+            Console.WriteLine("4.Digit Sum"); //Done
             Console.WriteLine("5.Palindrome Number");
-
-            _choice = int.Parse(Console.ReadLine());
-
-            int dividers;
-
-            if (_choice == 2)
-            {
-                Prime number = new Prime();
-                Console.WriteLine("Please enter a number to check whether is Prime or not");
-                int _number = int.Parse(Console.ReadLine());
-                number.IsPrime(_number);
-            }
+            Console.WriteLine("6.Factorial"); //Done
+            Console.WriteLine("7.Decimal to Binary");
         }
     }
 }
