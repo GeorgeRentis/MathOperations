4f2b158254c7c673ba0eb5f8fa2e12d4d3d568b8
geore <geore@LAPTOP-G6MS9ON9>
2020-03-01 20:07:22 +0200

    added operations file

---
+++ MathOperations/Prime.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MathOperations
+{
+    class Prime
+    {
+
+        public void IsPrime(int number)
+        {
+            int Dividers = 0;
+
+            for (int i = 1; i <= number; i++)
+            {
+                if (number % i ==0)
+                {
+                    Dividers++;
+                }
+            }
+
+           if (Dividers > 2)
+            {
+                Console.WriteLine("The number " + number + " is not a Prime number");
+            }
+            else
+            {
+                Console.WriteLine("The number " + number + "is a Prime number");
+            }
+        }
+    }
+}
---
+++ MathOperations/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MathOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Users choice
+            int _choice;
+            // Select Mathematical Operation
+            Console.WriteLine("Please select a number for your corresponding operation\n");
+            Console.WriteLine("1.Fibbonacci Series");
+            Console.WriteLine("2.Prime Numbers");
+            Console.WriteLine("3.Armstrong Number");
+            Console.WriteLine("4.Digit Sum");
+            Console.WriteLine("5.Palindrome Number");
+
+            _choice = int.Parse(Console.ReadLine());
+
+            int dividers;
+
+            if (_choice == 2)
+            {
+                Prime number = new Prime();
+                Console.WriteLine("Please enter a number to check whether is Prime or not");
+                int _number = int.Parse(Console.ReadLine());
+                number.IsPrime(_number);
+            }
+        }
+    }
+}
